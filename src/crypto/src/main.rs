01b060dd3dc0839f847bc0584625593d0e1feb0d
Bailey Collins <103609609+Bailey7220@users.noreply.github.com>
2025-09-23 14:57:36 -0500

    Update main.rs

--- src/crypto/src/main.rs
+++ src/crypto/src/main.rs
@@ -1,7 +1,7 @@
 //! QRAIOP Quantum-Safe Cryptography Demo
 
-use qraiop_crypto::{KeyEncapsulation, init, info};
 use qraiop_crypto::pqc::kyber::MlKem512;
+use qraiop_crypto::{info, init, KeyEncapsulation};
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -11,8 +11,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     let (pk, sk) = MlKem512::keypair()?;
     let (ct, _ss1) = MlKem512::encapsulate(&pk)?;
     let _ss2 = MlKem512::decapsulate(&sk, &ct)?;
-    
+
     println!("ML-KEM-512 roundtrip successful");
-    
+
     Ok(())
 }
